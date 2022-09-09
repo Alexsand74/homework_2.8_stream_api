ad901e30d574e7af920f09c626fdda41c7054628
Alexsand74 <1974alexsand@gmail.com>
2022-09-09 20:14:09 +0300

    2.8. Stream API и Optional

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This master project lies on a branch!");
+        System.out.println("This master project lies on a branch!!!");
     }
 }
